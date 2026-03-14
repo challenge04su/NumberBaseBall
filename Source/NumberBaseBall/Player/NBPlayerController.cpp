1f036789ad640b1e55caa9daa911f71e86f08ba3
josukyoung <challenge04su@gmail.com>
2026-03-14 13:54:14 +0900

    Input and Output
    h / cpp

---
+++ Source/NumberBaseBall/Player/NBPlayerController.cpp
@@ -0,0 +1,31 @@
+#include "NBPlayerController.h"
+#include "UI/NBChatInput.h"
+#include "Kismet/KismetSystemLibrary.h"
+void ANBPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+
+	if (IsValid(ChatInputWidgetClass) == true)
+	{
+		ChatInputWidgetInstance = CreateWidget<UNBChatInput>(this, ChatInputWidgetClass);
+		if (IsValid(ChatInputWidgetInstance) == true)
+		{
+			ChatInputWidgetInstance->AddToViewport();
+		}
+	}
+}
+
+void ANBPlayerController::SetChatMessageString(const FString& InChatMessageString)
+{
+	ChatMessageString = InChatMessageString;
+
+	PrintChatMessageString(ChatMessageString);
+}
+
+void ANBPlayerController::PrintChatMessageString(const FString& InChatMessageString)
+{
+	UKismetSystemLibrary::PrintString(this, ChatMessageString, true, true, FLinearColor::Yellow, 10.0f);
+}
---
+++ Source/NumberBaseBall/Player/NBPlayerController.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NBPlayerController.generated.h"
+
+class UNBChatInput;
+
+UCLASS()
+class NUMBERBASEBALL_API ANBPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+	void SetChatMessageString(const FString& InChatMessageString);
+	void PrintChatMessageString(const FString& InChatMessageString);
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UNBChatInput> ChatInputWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UNBChatInput> ChatInputWidgetInstance;
+
+	FString ChatMessageString;
+};
